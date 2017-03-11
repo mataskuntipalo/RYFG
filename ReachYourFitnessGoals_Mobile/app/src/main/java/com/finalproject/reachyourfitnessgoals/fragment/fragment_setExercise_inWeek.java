72b22285949a01bd391d6be87adbe59f1c811c85
ryfg <ryfgcs402@gmail.com>
2017-03-11 19:25:23 +0700

    create new fragment (setExercise in week)

---
+++ ReachYourFitnessGoals_Mobile/app/src/main/java/com/finalproject/reachyourfitnessgoals/fragment/fragment_setExercise_inWeek.java
@@ -0,0 +1,32 @@
+package com.finalproject.reachyourfitnessgoals.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.finalproject.reachyourfitnessgoals.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragment_setExercise_inWeek extends Fragment {
+
+
+    public fragment_setExercise_inWeek() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_set_exercise_in_week, container, false);
+
+        return rootview;
+    }
+
+}
